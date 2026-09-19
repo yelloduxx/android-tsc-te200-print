873685cf4a7bc2c0ad95ae17b9fdf0a779df5352
Duxxie <yelloduxx@users.noreply.github.com>
2026-09-19 19:08:53 +0300

    Bump app version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.tscprint"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
     }
 
     buildTypes {
